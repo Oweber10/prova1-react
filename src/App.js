a6e316e3518cbbef0178f3cd6ef020d80e58a31b
Gabriel Weber <lematraca@gmail.com>
2022-10-04 23:51:10 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,57 @@
-import logo from './logo.svg';
+import React from 'react'
 import './App.css';
+
+var carro = [
+  {cor:"preto",marca:"volkswagen",modelo:"fusca"},
+  {cor:"amarelo",marca:"volkswagen",modelo:"brasilia"},
+  {cor:"prata",marca:"mitsubishi",modelo:"lancer"},
+  {cor:"preto",marca:"honda",modelo:"HR-V"},
+  {cor:"amarelo",marca:"ford",modelo:"mustang"},
+  {cor:"prata",marca:"honda",modelo:"city"},
+  {cor:"vermelho",marca:"ford",modelo:"fiesta"}
+]
+
+//FAZER ISSO LER TODOS OS VETORES
+function leVetorQ1(carro){
+  for(let i in carro){
+    return(
+      <div>
+        Modelo: {carro[i].modelo}, Marca: {carro[i].marca}, Cor: {carro[i].cor}; <br/> 
+      </div>
+    )
+}
+}
+
+const estilo = {
+      body:{
+        backgroundColor:"rgb(46, 88, 112)",
+        with:"100%",
+        height:"1920px",
+      },
+
+      titulo:{
+        color:"whiteSmoke",
+        textAlign:'center',
+        FontFamily:"Arial"
+      },
+
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <body className="App" style={estilo.body}>
+      <div>
+        <h2 class="titulo" style={estilo.titulo}>PROVA 1 DE PROGRAMÇÃO ORIENTADA A EVENTOS</h2>
+        <p>Por: Hiury e Gabriel Weber</p>
+      </div>
+      <div class="q1">
+        <h1>Questão 1:</h1>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          vetor escolhido: <br/>
+          {leVetorQ1(carro)}
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      </div>
+    </body>
   );
 }
 
